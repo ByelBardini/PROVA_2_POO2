dd93a2049b66a990e21c0bbb13539facc06e4b9e
ByelBg <gabryel.bgorleans@gmail.com>
2024-05-06 19:51:19 -0300

    preparação projeto

--- src/prova_2/Conexao.java
+++ src/prova_2/Conexao.java
@@ -1,2 +1,23 @@
-package prova_2;public class Conexao {
+package prova_2;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class Conexao {
+    public static Connection concectaMySql(){
+        Connection conn = null;
+        try{
+            Properties props = new Properties();
+            props.load(new FileInputStream("db.properties"));
+
+            conn = DriverManager.getConnection(
+                    props.getProperty("url"),
+                    props);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
--- src/prova_2/tests/ConexaoTest.java
+++ src/prova_2/tests/ConexaoTest.java
@@ -1,4 +1,14 @@
+package prova_2.tests;
+
+import org.junit.jupiter.api.Test;
+import prova_2.Conexao;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConexaoTest {
-  
+
+    @Test
+    void conectaTest(){
+        assertNotNull(Conexao.concectaMySql());
+    }
 }
